7bd76f70794e55e69c2fc996baab8f70648ce8f8
Cameron <magicat8bill@gmail.com>
2024-09-24 09:06:04 -0600

    temp

--- cres-test.py
+++ cres-test.py
@@ -2,7 +2,13 @@ from music21 import *
 s = stream.Stream()
 c4 = note.Note("C4", quarterLength=1)
 d4 = note.Note("D4", quarterLength=2)
-s.append(c4)
-s.append(d4)
+e4 = note.Note('E4', quarterLength=3)
+d = dynamics.Crescendo()
+d.spread = 2
+d.placement = 'above'
+s.insert(0, note.Note('C4', quarterLength=1))
+s.insert(2, note.Note('D4', quarterLength=2))
+s.insert(4, note.Note('E4', quarterLength=3))
+s.insert(1, d)
 s.show()
-s.write('musicxml', 'my_music.xml')
+# s.write('musicxml', 'my_music.xml')
