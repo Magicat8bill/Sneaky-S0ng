92192ee76e7e49361728f6066265d1ee106b06b1
Koen Kleifgen <Corn.Bunny96@gmail.com>
2024-09-25 15:12:46 -0600

    deleted old text

--- word_half.py
+++ word_half.py
@@ -1,11 +1,3 @@
-"""my_list = ["r", "a", "b", "b", "i", "t"]
-chunk_size = 2
-split_list = [[] for _ in range(chunck size)]
-for index, value in enumerate(my_list):
-    sublist_index = index % chunk_size
-    split_list[sublist_index].append(value)
-print (split_list)"""
-
 def split_word(word):
     length = len(word)
     #floors the odd words so the second half will be longer
@@ -13,8 +5,7 @@ def split_word(word):
     first_half = word[:mid]
     second_half = word[mid:]
     return first_half, second_half
-
-word = "the"
+word = "suppercalafragilisticexpealedoshous"
 first_half, second_half = split_word(word)
 print("First half:", first_half)
 print("Second half:", second_half)
