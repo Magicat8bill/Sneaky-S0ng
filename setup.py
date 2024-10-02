a4fb7545751b8ac58e0343bc3a66a79b71f971ed
Cameron <magicat8bil@gmail.com>
2024-10-02 13:30:15 -0600

    Testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# from setuptools import setup
+from setuptools import setup
 
-# setup(
-#     name='sneaky-s0ng',
-#     version='2.0.1',
-#     py_modules=['sneaky-s0ng']
-# )
+setup(
+    name='sneaky-s0ng',
+    version='2.0.1',
+    py_modules=['sneaky-s0ng']
+)
