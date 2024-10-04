d6f6198ed7452f2ee83eedce884740c8180e1300
Cameron <magicat8bill@gmail.com>
2024-10-04 13:34:40 -0600

    updates

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='sneaky_s0ng',
     version='0.1',
-    py_modules=['music'],
+    py_modules=['music'],  # The module file should be music.py
     entry_points={
         'console_scripts': [
-            'sneaky_s0ng=sneaky_s0ng:main',
+            'sneaky_s0ng=music:main',  # Assuming music.py has a main() function
         ],
     },
 )
